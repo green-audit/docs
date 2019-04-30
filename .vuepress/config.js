ddae60df14de7a4ccc22c78610bea13b4ed56db7
耐小心 <qiqizjl@qq.com>
2019-05-01 03:25:24 +0800

    update

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,7 +1,17 @@
 module.exports = {
-    plugins: {
-        '@vuepress/pwa': {
-            serviceWorker: true,
-        }
-    }
+    plugins: [
+       
+        [
+            '@vuepress/pwa', {
+                serviceWorker: true,
+            }
+        ],
+        [
+            '@vuepress/google-analytics',
+            {
+                'ga': 'UA-73440236-2'
+            }
+        ]
+    ]
+
 }
